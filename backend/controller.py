b24084699c022da57d543018d5038187061eeb27
OwaisNoor000 <mowaisnoor000@gmail.com>
2026-01-03 14:49:10 +0200

    added allowed origin

--- backend/controller.py
+++ backend/controller.py
@@ -16,7 +16,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173","http://16.16.185.51:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
